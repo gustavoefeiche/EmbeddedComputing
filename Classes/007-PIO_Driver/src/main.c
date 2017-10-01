57440d71d4ddf3a4f7b931ee6f0c39c68125c367
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2017-09-30 23:08:09 -0300

    Add template for all main.c

--- Classes/007-PIO_Driver/src/main.c
+++ Classes/007-PIO_Driver/src/main.c
@@ -1,40 +1,21 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
+/************************************************************************/
+/* Aula 007 - PIO Driver                                                */
+/* Autor: Gustavo Efeiche                                               */
+/* Data: 30/09/2017                                                     */
+/************************************************************************/
 
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
+/************************************************************************/
+/* INCLUDES                                                             */
+/************************************************************************/
 #include <asf.h>
 
+/************************************************************************/
+/* MAIN CODE                                                            */
+/************************************************************************/
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
-
+	sysclk_init();
 	board_init();
 
-	/* Insert application code here, after the board has been initialized. */
+	while(1) {}
 }
